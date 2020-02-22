6f148ed6b3e9222049831323bdc9a7b99d8abff5
Raphael <raphael.pestourie@gmail.com>
2020-02-22 15:46:09 -0500

    check that results are consistent, perf (speed, core): (x2, 4), (x4, 6)

--- examples/serial_simulations.jl
+++ examples/serial_simulations.jl
@@ -1,8 +1,11 @@
 include("../src/fdfd_local_field.jl")
+cd("examples/")
 
+using BenchmarkTools
 using DelimitedFiles
 
-number_training_data = 10 # ideally 1e5 here
+println("$(Threads.nthreads()) threads!")
+number_training_data = 10000 # ideally 1e5 here
 
 r = rand()
 fname = "test_data_$(round(Int, r*1e5)).csv"
@@ -21,17 +24,18 @@ ref_local_field = get_local_field(Float64[0.5], refractive_indexes=refractive_in
 # simulations
 refractive_indexes_sim = Float64[1.0, 1.0, 1.45]
 # TODO: embarassingly parallel code to parallelize
-sim_local_fields = ComplexF32[get_local_field(ps_array[it_ps, :], refractive_indexes=refractive_indexes_sim)[3] for it_ps=1:size(ps_array)[1]]
-
-using BenchmarkTools
-
-# @btime sim_local_fields = ComplexF32[get_local_field(ps_array[it_ps, :],
-# refractive_indexes=refractive_indexes_sim)[3] for it_ps=1:size(ps_array)[1]]
+# @btime sim_local_fields = ComplexF32[get_local_field(ps_array[it_ps, :], refractive_indexes=refractive_indexes_sim)[3] for it_ps=1:size(ps_array)[1]]
+#
+# TODO: create a test with this assertion
+# sim_local_fields2 = zeros(ComplexF64, (number_holes+1)*number_training_data)
+# @btime @inbounds Base.Threads.@threads for it_ps=1:size(ps_array)[1]
+#     sim_local_fields2[it_ps] = get_local_field(ps_array[it_ps, :], refractive_indexes=refractive_indexes_sim)[3]
+# end
+# @assert all(sim_local_fields .≈ sim_local_fields2)
 
 sim_local_fields = zeros(ComplexF64, (number_holes+1)*number_training_data)
-@btime @inbounds Base.Threads.@threads for it_ps=1:size(ps_array)[1]
+@inbounds Base.Threads.@threads for it_ps=1:size(ps_array)[1]
     sim_local_fields[it_ps] = get_local_field(ps_array[it_ps, :], refractive_indexes=refractive_indexes_sim)[3]
 end
-
 local_field_data = sim_local_fields./ref_local_field
-writedlm("examples/data/"*fname,hcat(ps_array, real.(local_field_data), imag.(local_field_data)), ',')
+writedlm("./data/"*fname,hcat(ps_array, real.(local_field_data), imag.(local_field_data)), ',')
